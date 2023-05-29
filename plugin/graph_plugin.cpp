612a535da4c89b3b2c76fe0b83f91bc0311d1067
Evgeny Shtanov <shtanov_evgenii@mail.ru>
2023-05-29 17:55:00 +0300

    [refact] Little refactoring
    Signed-off-by: Evgeny Shtanov <shtanov_evgenii@mail.ru>

--- plugin/graph_plugin.cpp
+++ plugin/graph_plugin.cpp
@@ -145,7 +145,7 @@ bool GraphPlugin::loadJSONs(QStringList subdirsNames)
                 }
 
                 // Read JSON for Table and it's model
-                loadTableJSON(QString("%1/%2/%3/%4").arg(allConfigsFolder).arg(dirName).arg(pluginConfigsFolder).arg("plugin_config.json"), dirName.toUpper());
+                loadTableJSON(path, dirName.toUpper());
 
                 // Read JSON for Vector Indicators Board description
                 auto indPath = QString("%1/%2/%3/%4").arg(allConfigsFolder).arg(dirName).arg(pluginConfigsFolder).arg(arrayIndicatorsJson);
