309effc73e0abe6513f23a3165619d6349db1cff
Evgeny Shtanov <shtanov_evgenii@mail.ru>
2024-03-16 13:41:49 +0300

    Save
    Signed-off-by: Evgeny Shtanov <shtanov_evgenii@mail.ru>

--- plugin/qcphaircomb.cpp
+++ plugin/qcphaircomb.cpp
@@ -1,7 +1,27 @@
 #include "qcphaircomb.h"
 
-QCPHairComb::QCPHairComb(QCPAxis *keyAxis, QCPAxis *valueAxis)
-    : QCPAbstractPlottable1D<QLine>(keyAxis, valueAxis)
+QCPHairComb::QCPHairComb(QCustomPlot *cplot) // QCPAxis *keyAxis, QCPAxis *valueAxis)
+    : QCPAbstractPlottable1D<QCPVectorData>(cplot->xAxis, cplot->yAxis)
 {
-QCPGraph;
+    QCPGraphDataContainer points1, points2;
+    m_scatter1 = cplot->addGraph(cplot->xAxis, cplot->yAxis);
+    m_scatter1->setLineStyle(QCPGraph::lsNone);
+    m_scatter1->setScatterStyle(QCPScatterStyle::ssTriangle);
+    m_scatter2 = cplot->addGraph(cplot->xAxis, cplot->yAxis);
+    m_scatter2->setLineStyle(QCPGraph::lsNone);
+    m_scatter2->setScatterStyle(QCPScatterStyle::ssDot);
+}
+
+void QCPHairComb::addData(const QVector<double> &keys1, const QVector<double> &values1,
+                          const QVector<double> &keys2, const QVector<double> &values2,
+                          bool alreadySorted0)
+{
+    m_scatter1->addData(keys1, values1, alreadySorted0);
+    m_scatter2->addData(keys2, values2, alreadySorted0);
+}
+
+void QCPHairComb::addData(double key1, double value1, double key2, double value2)
+{
+    m_scatter1->addData(key1, value1);
+    m_scatter2->addData(key2, value2);
 }
--- plugin/qcphaircomb.h
+++ plugin/qcphaircomb.h
@@ -11,14 +11,14 @@ public:
   QCPVectorData();
   QCPVectorData(double key, double value);
 
-  inline double sortKey() const { return key; }
+  inline double sortKey() const { return keyVal1.first; }
   inline static QCPGraphData fromSortKey(double sortKey) { return QCPGraphData(sortKey, 0); }
   inline static bool sortKeyIsMainKey() { return true; }
 
-  inline double mainKey() const { return key; }
-  inline double mainValue() const { return value; }
+  inline double mainKey() const { return keyVal1.first; }
+  inline double mainValue() const { return keyVal1.second; }
 
-  inline QCPRange valueRange() const { return QCPRange(value, value); }
+  inline QCPRange valueRange() const { return QCPRange(keyVal1.second, keyVal1.second); }
 
   // double key, value;
   // QPointF start, end;
@@ -26,11 +26,54 @@ public:
 };
 Q_DECLARE_TYPEINFO(QCPVectorData, Q_PRIMITIVE_TYPE);
 
-class QCPHairComb : public QCPAbstractPlottable1D<QLine>
+
+class QCPHairCombGraph : public QCPGraph
 {
     Q_OBJECT
 public:
-    QCPHairComb(QCPAxis *keyAxis, QCPAxis *valueAxis);
+    QCPHairCombGraph(QCPAxis *keyAxis, QCPAxis *valueAxis);
+
+    void addData(const QVector<double> &keys1, const QVector<double> &values1,
+                 const QVector<double> &keys2, const QVector<double> &values2,
+                 bool alreadySorted=false);
+    void addData(double key1, double value1, double key2, double value2);
+
+private:
+
+};
+
+
+class QCPHairComb : public QCPAbstractPlottable1D<QCPVectorData>
+{
+    Q_OBJECT
+public:
+    QCPHairComb(QCustomPlot *cp = nullptr); // QCPAxis *keyAxis, QCPAxis *valueAxis);
+
+    // virtual methods of 1d plottable interface:
+    virtual int dataCount() const override;
+    virtual double dataMainKey(int index) const override;
+    virtual double dataSortKey(int index) const override;
+    virtual double dataMainValue(int index) const override;
+    virtual QCPRange dataValueRange(int index) const override;
+    virtual QPointF dataPixelPosition(int index) const override;
+    virtual bool sortKeyIsMainKey() const override;
+    virtual QCPDataSelection selectTestRect(const QRectF &rect, bool onlySelectable) const override;
+    virtual int findBegin(double sortKey, bool expandedRange=true) const override;
+    virtual int findEnd(double sortKey, bool expandedRange=true) const override;
+
+    // reimplemented virtual methods:
+    virtual double selectTest(const QPointF &pos, bool onlySelectable, QVariant *details=nullptr) const override;
+    virtual QCPPlottableInterface1D *interface1D() override { return this; }
+    virtual void setMaxCount(int count);
+    void clear();
+
+    void addData(const QVector<double> &keys1, const QVector<double> &values1,
+                 const QVector<double> &keys2, const QVector<double> &values2,
+                 bool alreadySorted=false);
+    void addData(double key1, double value1, double key2, double value2);
+
+private:
+    QCPGraph *m_scatter1, *m_scatter2;
 };
 
 #endif // QCPHAIRCOMB_H
